201d35c132796436c8587fc70b3ceabca69545f3
cspotz <66311976+cspotz@users.noreply.github.com>
2021-08-23 10:56:55 +0200

    Add files via upload

---
+++ poisson_commons.f90
@@ -0,0 +1,36 @@
+module poisson_commons
+  use amr_commons
+  use poisson_parameters
+
+  real(dp),allocatable,dimension(:)  ::phi,phi_old       ! Potential
+  real(dp),allocatable,dimension(:)  ::rho               ! Density
+  real(dp),allocatable,dimension(:,:)::f                 ! 3-force
+
+  real(dp),allocatable,dimension(:)  ::rho_top   ! Density at last CIC level
+
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+  real(dp),allocatable,dimension(:)  ::phi_m,phi_m_old       ! Potential "negative mass"
+  real(dp),allocatable,dimension(:)  ::rho_m                 ! Density "negative mass"
+  real(dp),allocatable,dimension(:,:)::f_m                   ! 3-force "negative mass"
+
+  real(dp),allocatable,dimension(:)  ::rho_m_top   ! Density at last CIC level "negative mass"
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+  ! Multigrid lookup table for amr -> mg index mapping
+  integer, allocatable, dimension(:) :: lookup_mg   ! Lookup table
+
+  ! Communicator arrays for multigrid levels
+  type(communicator), allocatable, dimension(:,:) :: active_mg
+  type(communicator), allocatable, dimension(:,:) :: emission_mg
+
+  ! Minimum MG level
+  integer :: levelmin_mg
+
+  ! Multigrid safety switch
+  logical, allocatable, dimension(:) :: safe_mode
+
+  ! Multipole coefficients
+  real(dp),dimension(1:ndim+1)::multipole
+
+end module poisson_commons
